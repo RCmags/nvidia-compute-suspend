b6b7406714dcc7008dbfa852b608b3b511d0d588
RCmags <memoryofatrufestival@gmail.com>
2025-12-28 21:24:37 -0600

    further refined scripts; moved instructions to readme

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
-# Nvidia Compute Killer | Safe eGPU suspend on Linux
+# Nvidia GPU Compute Killer | Safe suspend for eGPU's on Linux
 Linux Systemd service that kills any service that is using an Nvidia GPU's compute. This prevents
 eGPU's from causing the computer to crash when suspending. Intended to be used with older laptops
 using the "EXP GDC Beast" eGPU via express card.
 
-# Test
-Test the service by running:
+# Installation 
+
+**nvidia-compute-suspend.sh**:
+This script kills all nvidia GPU compute processes to prevent crashes when suspending an eGPU. 
+Place at: `/usr/local/bin/nvidia-compute-suspend.sh`    
+
+Then run:
+```bash
+sudo chmod +x /usr/local/bin/nvidia-compute-suspend.sh
+```
+
+**nvidia-compute-suspend.service**:
+Place the service at: `/etc/systemd/system/nvidia-compute-suspend.service`    
+
+Then run:
+```bash
+sudo systemctl enable nvidia-compute-suspend.service
+sudo systemctl start nvidia-compute-suspend.service
+```
+
+# Output
+Suspend your computer then test the service by running:
 
 ```bash
 sudo journalctl -xeu nvidia-compute-suspend.service
 ```
 
-Then checking the output messages.
+Then check the output messages. They should indicate the proceeses that were killed when the
+computer was suspended.
--- nvidia-compute-suspend.sh
+++ nvidia-compute-suspend.sh
@@ -1,39 +1,23 @@
 #!/bin/bash
 
-#---- DESCRIPTION -----------------------------------------
-# This script kills all nvidia GPU compute processes 
-# to prevent crashes when suspending an eGPU. 
-
-# Place at: 
-# /usr/local/bin/nvidia-compute-suspend.sh
-
-# Then run:
-# sudo chmod +x /usr/local/bin/nvidia-compute-suspend.sh
-#----------------------------------------------------------
-
-# Ensure video card is connected
-# NOTE: driver must be checked BEFORE listing GPUs
-check_driver_exists() {
-    !(command -v nvidia-smi &> /dev/null) # get exit code
-}
-
-check_gpu_exists() {
-    !(nvidia-smi -L &> /dev/null) # get exit code
-}
-
-if check_driver_exists && check_gpu_exists; then
-    echo "Nvidia GPU NOT detected. Exiting"
-    exit 0
-fi
-
-# Processes using the GPU
-pids=$(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
-
-# Stop everything to clear VRAM
-if [ -n "$pids" ]; then
-    echo "Killing compute processes:"
-    echo $(nvidia-smi --query-compute-apps=name --format=csv,noheader) # print names
+# Ensure Nvidia GPU is present
+if [ nvidia-smi -L &> /dev/null ]; then
     
-    echo "$pids" | xargs -r kill -SIGKILL   # Kill immediately
+    # Processes using the GPU
+    pids=$(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
+    
+    if [ -n "$pids" ]; then
+       # print names
+       echo "Killing:"
+       echo $(nvidia-smi --query-compute-apps=name --format=csv,noheader) 
+       
+       # Stop processes immediately
+       echo "$pids" | xargs -r kill -SIGKILL
+       sleep 1  # Wait to prevent anything weird
+    else
+       echo "No processes to kill"
+    fi
+    
+else
+    echo "No nvidia GPU detected"
 fi
-
